0b962a42e8fa4857ef3cb5cf86ae05fe3dd26175
my0366 <rlacnrrnrhd@naver.com>
2025-03-13 11:41:52 +0900

    Remove Shadcn UI Package

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_template/app/app_provider.dart';
 import 'package:flutter_template/app/routes.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ShadApp.custom(
-      themeMode: ref.watch(appProvider),
-      darkTheme: ShadThemeData(
-        brightness: Brightness.dark,
-        colorScheme: ShadColorScheme.fromName('orange', brightness: Brightness.dark),
+    return MaterialApp.router(
+      builder: (context, child) => Overlay(
+        initialEntries: [
+          if (child != null)
+            OverlayEntry(
+              builder: (context) => child,
+            ),
+        ],
       ),
-      theme: ShadThemeData(brightness: Brightness.dark, colorScheme: ShadColorScheme.fromName('orange')),
-      appBuilder: (context, theme) {
-        return MaterialApp.router(
-          routerConfig: router,
-          theme: theme,
-          builder: (context, child) {
-            return ShadToaster(child: child!);
-          },
-        );
-      },
+      routerConfig: router,
+      supportedLocales: const [
+        Locale('ko'),
+      ],
+      restorationScopeId: 'app',
     );
   }
 }
